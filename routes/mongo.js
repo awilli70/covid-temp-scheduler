733faa416caf938984adff4496ad92b4a605ced0
Alex Williams <54961598+awilli70@users.noreply.github.com>
2020-07-19 22:55:19 -0400

    Update mongo.js

--- routes/mongo.js
+++ routes/mongo.js
@@ -15,6 +15,10 @@ router.post('/updateTemp', async (req, res) => {
     client = req.client;
     const phone = req.body.phone;
     let temp = parseFloat(req.body.temp);
+    console.log(req.body.symptomOne)
+    console.log(req.body.symptomTwo)
+    console.log(req.body.symptomThree)
+    console.log(req.body.symptomFour)
     let taste = (parseInt(req.body.symptomOne) === 1 || regex.test(req.body.symptomOne));
     let cough = (parseInt(req.body.symptomTwo) === 1 || regex.test(req.body.symptomTwo));
     let fever = (parseInt(req.body.symptomThree) === 1 || regex.test(req.body.symptomThree));
